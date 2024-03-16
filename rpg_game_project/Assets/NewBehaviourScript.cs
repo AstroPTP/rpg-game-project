64aaa24d46e07a345732119d174b5c85158b8b39
Astro <phuphan.work@gmail.com>
2024-03-17 00:33:28 +0800

    Initial commit

---
+++ rpg_game_project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.color = Color.red; 
+    }
+
+    public Image image;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
